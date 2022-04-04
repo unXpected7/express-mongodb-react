9fce07596861030ac4a4ac72b105cb00b195e666
unXpected7 <ilyasyusuf002@gmail.com>
2022-04-04 11:00:34 +0700

    router v2

---
+++ express/routes/routerV2.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+const multer = require('multer');
+const upload = multer({dest: 'uploads'});
+const productController = require('../controllers/productsV2')
+
+router.post('/products', productController.storeProducts);
+router.get('/products', productController.findProducts)
+router.post('/products/:id', productController.hapus);
+
+
+module.exports = router;
